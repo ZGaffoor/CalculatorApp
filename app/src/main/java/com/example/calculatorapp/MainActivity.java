56fa9a329c6adee242579e05fa7a6afcc86133f6
ZGaffoor <90778249+ZGaffoor@users.noreply.github.com>
2022-08-23 09:27:15 -0500

    button fixed add button works

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.calculatorapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void findSum(View view) {
+        EditText valOne = (EditText)findViewById(R.id.valOne);
+        EditText valTwo = (EditText)findViewById(R.id.valTwo);
+        TextView numberSumTV = (TextView)findViewById(R.id.result);
+
+        int num1 = Integer.parseInt((valOne.getText().toString()));
+        int num2 = Integer.parseInt((valTwo.getText().toString()));
+        int sum = num1 + num2;
+
+        numberSumTV.setText("" + sum);
     }
 }
