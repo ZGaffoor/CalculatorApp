2a5afbe4a593fcddc079e78d2037447700b5c017
ZGaffoor <90778249+ZGaffoor@users.noreply.github.com>
2022-08-24 08:19:35 -0500

    all buttons functional and changed colors

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -31,4 +31,45 @@ public class MainActivity extends AppCompatActivity {
 
         numberSumTV.setText("" + sum);
     }
+
+    public void findDiff(View view) {
+        EditText valOne = (EditText)findViewById(R.id.valOne);
+        EditText valTwo = (EditText)findViewById(R.id.valTwo);
+        TextView numberSumTV = (TextView)findViewById(R.id.result);
+
+        int num1 = Integer.parseInt((valOne.getText().toString()));
+        int num2 = Integer.parseInt((valTwo.getText().toString()));
+        int diff = num1 - num2;
+
+        numberSumTV.setText("" + diff);
+    }
+
+    public void findProd(View view) {
+        EditText valOne = (EditText)findViewById(R.id.valOne);
+        EditText valTwo = (EditText)findViewById(R.id.valTwo);
+        TextView numberSumTV = (TextView)findViewById(R.id.result);
+
+        int num1 = Integer.parseInt((valOne.getText().toString()));
+        int num2 = Integer.parseInt((valTwo.getText().toString()));
+        int prod = num1 * num2;
+
+        numberSumTV.setText("" + prod);
+    }
+
+
+    public void findQuotient(View view) {
+        EditText valOne = (EditText)findViewById(R.id.valOne);
+        EditText valTwo = (EditText)findViewById(R.id.valTwo);
+        TextView numberSumTV = (TextView)findViewById(R.id.result);
+
+        int num1 = Integer.parseInt((valOne.getText().toString()));
+        int num2 = Integer.parseInt((valTwo.getText().toString()));
+        int quotient = num1 / num2;
+
+        numberSumTV.setText("" + quotient);
+    }
+
+
+
+
 }
