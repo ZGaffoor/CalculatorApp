8a8422c636cdb47051f98c9a5b4716430cbc7e4f
ZGaffoor <90778249+ZGaffoor@users.noreply.github.com>
2022-08-25 08:19:36 -0500

    added power and also made it so empty input doesnt crash

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -7,7 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -19,55 +19,96 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+    public void makeToast(String str){
+        Toast toast = Toast.makeText(getApplicationContext(),
+                "Please enter an int value", Toast.LENGTH_SHORT);
 
+        toast.show();
+
+    }
     public void findSum(View view) {
         EditText valOne = (EditText)findViewById(R.id.valOne);
         EditText valTwo = (EditText)findViewById(R.id.valTwo);
         TextView numberSumTV = (TextView)findViewById(R.id.result);
+        if (valOne.getText().toString().equals("") || valTwo.getText().toString().equals("")) {
+            makeToast("Enter an Int value");
 
-        int num1 = Integer.parseInt((valOne.getText().toString()));
-        int num2 = Integer.parseInt((valTwo.getText().toString()));
-        int sum = num1 + num2;
+        } else {
+            int num1 = Integer.parseInt((valOne.getText().toString()));
+            int num2 = Integer.parseInt((valTwo.getText().toString()));
+            int sum = num1 + num2;
 
-        numberSumTV.setText("" + sum);
+            numberSumTV.setText("" + sum);
+        }
+
     }
 
     public void findDiff(View view) {
         EditText valOne = (EditText)findViewById(R.id.valOne);
         EditText valTwo = (EditText)findViewById(R.id.valTwo);
         TextView numberSumTV = (TextView)findViewById(R.id.result);
+        if (valOne.getText().toString().equals("") || valTwo.getText().toString().equals("")) {
+            makeToast("Enter an Int value");
 
-        int num1 = Integer.parseInt((valOne.getText().toString()));
-        int num2 = Integer.parseInt((valTwo.getText().toString()));
-        int diff = num1 - num2;
+        } else {
+            int num1 = Integer.parseInt((valOne.getText().toString()));
+            int num2 = Integer.parseInt((valTwo.getText().toString()));
+            int diff = num1 - num2;
 
-        numberSumTV.setText("" + diff);
+            numberSumTV.setText("" + diff);
+        }
+
     }
 
     public void findProd(View view) {
-        EditText valOne = (EditText)findViewById(R.id.valOne);
-        EditText valTwo = (EditText)findViewById(R.id.valTwo);
-        TextView numberSumTV = (TextView)findViewById(R.id.result);
+        EditText valOne = (EditText) findViewById(R.id.valOne);
+        EditText valTwo = (EditText) findViewById(R.id.valTwo);
+        TextView numberSumTV = (TextView) findViewById(R.id.result);
+        if (valOne.getText().toString().equals("") || valTwo.getText().toString().equals("")) {
+            makeToast("Enter an Int value");
 
-        int num1 = Integer.parseInt((valOne.getText().toString()));
-        int num2 = Integer.parseInt((valTwo.getText().toString()));
-        int prod = num1 * num2;
+        } else {
 
-        numberSumTV.setText("" + prod);
+            int num1 = Integer.parseInt((valOne.getText().toString()));
+            int num2 = Integer.parseInt((valTwo.getText().toString()));
+            int prod = num1 * num2;
+
+            numberSumTV.setText("" + prod);
+        }
     }
-
 
     public void findQuotient(View view) {
         EditText valOne = (EditText)findViewById(R.id.valOne);
         EditText valTwo = (EditText)findViewById(R.id.valTwo);
         TextView numberSumTV = (TextView)findViewById(R.id.result);
+        if (valOne.getText().toString().equals("") || valTwo.getText().toString().equals("")) {
+            makeToast("Enter an Int value");
+        }else{
+            int num1 = Integer.parseInt((valOne.getText().toString()));
+            int num2 = Integer.parseInt((valTwo.getText().toString()));
+            int quotient = num1 / num2;
 
-        int num1 = Integer.parseInt((valOne.getText().toString()));
-        int num2 = Integer.parseInt((valTwo.getText().toString()));
-        int quotient = num1 / num2;
-
-        numberSumTV.setText("" + quotient);
+            numberSumTV.setText("" + quotient);
+        }
     }
+
+    public void findPower(View view) {
+        EditText valOne = (EditText)findViewById(R.id.valOne);
+        EditText valTwo = (EditText)findViewById(R.id.valTwo);
+        TextView numberSumTV = (TextView)findViewById(R.id.result);
+        if (valOne.getText().toString().equals("") || valTwo.getText().toString().equals("")) {
+            makeToast("Enter an Int value");
+        }else{
+            int num1 = Integer.parseInt((valOne.getText().toString()));
+            int num2 = Integer.parseInt((valTwo.getText().toString()));
+            double power = Math.pow(num1, num2);
+
+            numberSumTV.setText("" + power);
+        }
+    }
+
+
+
 
 
 
